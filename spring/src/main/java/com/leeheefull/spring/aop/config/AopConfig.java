a33ff85d55e4d4ffb2216fecd2b5c9e4c6634fca
leeheechan <leeheefull@gmail.com>
2022-06-30 23:41:25 +0900

    [spring] spring aop 간단 실행 예제 추가

---
+++ spring/src/main/java/com/leeheefull/spring/aop/config/AopConfig.java
@@ -0,0 +1,9 @@
+package com.leeheefull.spring.aop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+public class AopConfig {
+}
