6309edeca51c12aaa3d202046d307e9aa14da9ab
leeheechan <leeheefull@gmail.com>
2022-06-30 23:47:19 +0900

    [spring] spring scheduler 간단 실행 예제 추가

---
+++ spring/src/main/java/com/leeheefull/spring/scheduler/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.leeheefull.spring.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfig {
+}
