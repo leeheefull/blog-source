b535fb0a41128fe1aef18e5eb4d85193dc62788a
leeheechan <leeheefull@gmail.com>
2022-07-17 23:46:21 +0900

    [spring-data] complete querydsl setting

--- spring-data/src/test/java/com/leeheefull/springdata/SpringDataApplicationTests.java
+++ spring-data/src/test/java/com/leeheefull/springdata/SpringDataApplicationTests.java
@@ -1,13 +1,38 @@
 package com.leeheefull.springdata;
 
+import com.leeheefull.springdata.entity.Hello;
+import com.leeheefull.springdata.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class SpringDataApplicationTests {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result)
+                .isEqualTo(hello);
     }
 
 }
